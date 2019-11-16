3d249f1ba218faf45b07b5f539fc38aa4ba28e1e
Jonathan Gravato <jonathangravato@jonathans-mbp.home>
2019-11-16 15:58:04 -0500

    Added new components into App and added all hard-coded books to testing object array

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,35 +1,17 @@
 import React, { Component } from 'react'
+import Book from './Book'
 
 class BookShelf extends Component {
   render() {
     //console.log('Props', this.props)
+
     return (
-      <ol className="books-grid">
-        {this.props.books.map((book) => (
-          <li key={book.title}>
-            <div className="book">
-              <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url('${book.imageLinks.thumbnail}')` }}></div>
-              <div className="book-shelf-changer">
-                  <select>
-                  <option value="move" disabled>Move to...</option>
-                  <option value="currentlyReading">Currently Reading</option>
-                  <option value="wantToRead">Want to Read</option>
-                  <option value="read">Read</option>
-                  <option value="none">None</option>
-                  </select>
-              </div>
-              </div>
-              <div className="book-title">{book.title}</div>
-              <div className="book-authors">
-                {book.authors.map((author) => (
-                  author
-                ))}
-              </div>
-            </div> 
-          </li>
-        ))}  
-      </ol>
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
+        <div className="bookshelf-books">
+          <Book books={this.props.books} />
+        </div>
+      </div>
     )
   }
 }
