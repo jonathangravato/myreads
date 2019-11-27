8031dbe5458914b081c09d76f47db9882412523d
Jonathan Gravato <jonathangravato@jonathans-mbp.home>
2019-11-27 12:07:49 -0500

    Sorted books in App.js and passed to ListBooks for rendering - issues with looping shelves of books

--- src/App.js
+++ src/App.js
@@ -15,9 +15,26 @@ class App extends React.Component {
        * pages, as well as provide a good URL they can bookmark and share.
        */
       showSearchPage: false,
-      books: []
+      books: null,
+      sortedBooks: [
+        {
+          shelfReading: {
+            title: '',
+            books: null
+          },
+          shelfWantRead: {
+            title: '',
+            books: null
+          },
+          shelfRead: {
+            title: '',
+            books: null
+          }
+        }
+      ]
     }
-
+    
+    this.sortBooks = this.sortBooks.bind(this)
     this.toggleSearchPage = this.toggleSearchPage.bind(this)
   }
 
@@ -25,9 +42,42 @@ class App extends React.Component {
     this.setState({ showSearchPage: false })
   }
 
+  sortBooks(books) {
+
+    const shelfReading = []
+    const shelfWantRead = []
+    const shelfRead = []
+
+    books.map((book) => {
+      book.shelf === 'currentlyReading' && shelfReading.push(book)
+      book.shelf === 'wantToRead' && shelfWantRead.push(book)
+      book.shelf === 'read' && shelfRead.push(book)
+      return books
+    })
+
+    this.setState(() => ({
+      sortedBooks: {
+        shelfReading: {
+          title: 'Currently Reading',
+          books: shelfReading
+        },
+        shelfWantRead: {
+          title: 'Want to Read',
+          books: shelfWantRead
+        },
+        shelfRead: {
+          title: 'Read',
+          books: shelfRead
+        }
+      }
+    }))
+  }
+
   componentDidMount() {
     //getAll from BooksAPI
     BooksAPI.getAll().then((books) => {
+      // Sort books into shelves to be passed to list books
+      this.sortBooks(books)
       this.setState(() => ({
         books
       }))
@@ -46,7 +96,10 @@ class App extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <ListBooks books={this.state.books}/>
+                <ListBooks 
+                  books = {this.state.books}
+                  shelves = {this.state.sortedBooks}
+                />
               </div>
             </div>
             <div className="open-search">
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,75 +1,48 @@
 import React, { Component } from 'react'
 
 class ListBooks extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        shelfReading: {
-          title: 'Currently Reading',
-          books: []
-        },
-        shelfWantRead: {
-          title: 'Want to Read',
-          books: []
-        },
-        shelfRead: {
-          title: 'Read',
-          books: []
-        },
-        sortedBooks: []
-      }
-
-      //this.handleChange = this.handleChange.bind(this);
-      //this.sortBooks = this.sortBooks.bind(this)
-    }
-
-    componentDidUpdate() {
-        // eslint-disable-next-line array-callback-return
-        this.props.books.map((book) => {
-          book.shelf === 'currentlyReading' && this.state.shelfReading.books.push(book)
-          book.shelf === 'wantToRead' && this.state.shelfWantRead.books.push(book)
-          book.shelf === 'read' && this.state.shelfRead.books.push(book)
-        })
-        
-        this.setState((state) => ({
-          sortedBooks: [...this.state.shelfReading.books, ...this.state.shelfWantRead.books, ...this.state.shelfRead.books]
-        }))
-    }
-    
     render() {
-      return (
-        <div className="bookshelf">
-          <h2 className="bookshelf-title">Books in List</h2>
-          <div className="bookshelf-books">
-            <ol className="books-grid">
-                {this.props.books.map((book) => (
-                    <li key={book.title}>
-                        <div className="book">
-                        <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url('${book.imageLinks.thumbnail}')` }}></div>
-                        <div className="book-shelf-changer">
-                            <select onChange={this.updateShelf}>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                            </select>
-                        </div>
-                        </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">
-                            {book.authors.map((author) => (
-                            author
-                            ))}
-                        </div>
-                        </div> 
-                    </li>
-                ))}
-            </ol>
+      if(this.props.books){
+        return (
+          <div>
+              {Object.keys(this.props.shelves).map((shelf) =>  (
+                <div className="bookshelf" key={shelf.title}>
+                  <h2 className="bookshelf-title">{shelf.title}</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {/* {this.props.books.map((book) => (
+                            <li key={book.title}>
+                                <div className="book">
+                                <div className="book-top">
+                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url('${book.imageLinks.thumbnail}')` }}></div>
+                                <div className="book-shelf-changer">
+                                    <select onChange={this.updateShelf}>
+                                    <option value="move" disabled>Move to...</option>
+                                    <option value="currentlyReading">Currently Reading</option>
+                                    <option value="wantToRead">Want to Read</option>
+                                    <option value="read">Read</option>
+                                    <option value="none">None</option>
+                                    </select>
+                                </div>
+                                </div>
+                                <div className="book-title">{book.title}</div>
+                                <div className="book-authors">
+                                    {book.authors.map((author) => (
+                                    author
+                                    ))}
+                                </div>
+                                </div> 
+                            </li>
+                        ))} */}
+                    </ol>
+                  </div>
+                </div>
+              ))}
           </div>
-        </div>
-      )
+        )
+      } else {
+        return <div><p>Loadding...</p></div>
+      }
     }
   }
   
